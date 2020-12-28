1b2422479c0bb826a44d647b02411d105039fc23
Alex Liu <alexyliu@google.com>
2020-12-28 12:01:02 -0800

    blink rate graph working

--- index.js
+++ index.js
@@ -2,6 +2,32 @@ let timestamps = []
 let aggregatedTimestamps = []
 let ping = new sound("ping.mp3")
 
+var margin = {top: 40, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// Functions
+
 function sound(src) {
   this.sound = document.createElement("audio");
   this.sound.src = src;
@@ -27,9 +53,40 @@ function callEveryMinute() {
 }
 
 function drawChart() {
-  console.log("minute repeat")
   groupTimestamps()
   // Do things in D3
+  x.domain(aggregatedTimestamps.map(function(d) { return d.time; }));
+  y.domain([0, d3.max(aggregatedTimestamps, function(d) { return d.count; })]);
+
+  svg.selectAll("*").remove()
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Blink Count");
+
+  svg.selectAll(".bar")
+      .data(aggregatedTimestamps)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.time); })
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) { return y(d.count); })
+      .attr("height", function(d) { return height - y(d.count); })
+
+  function type(d) {
+    d.count = +d.count;
+    return d;
+  }
 }
 
 function groupTimestamps() {
