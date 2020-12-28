c1c0ac1d7166db7683c3a33dd6a260b3a6219ed1
Alex Liu <alexyliu@google.com>
2020-12-27 18:05:23 -0800

    add README

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+function distance(a, b) {
+  return Math.sqrt(Math.pow(a[0] - b[0], 2) + Math.pow(a[1] - b[1], 2));
+}
+
 async function setupCamera() {
   video = document.getElementById('video');
 
@@ -26,7 +30,18 @@ async function renderPrediction() {
   });
 
   if (predictions.length > 0) {
-    console.log(predictions)
+    // console.log(predictions)
+
+    // for points 145 and 159 in the scaled model,
+    // calculate the "close" event
+
+    if (distance(predictions[0].scaledMesh[159], predictions[0].scaledMesh[145]) < 8) {
+      console.log("blinked");
+
+      var circle = d3.select("circle");
+      var radius = circle.attr("r");
+      circle.attr("r", parseInt(radius) + 5);
+    }
 
     // for (let i = 0; i < predictions.length; i++) {
     //   const keypoints = predictions[i].scaledMesh;
@@ -46,10 +61,11 @@ async function renderPrediction() {
 async function main() {
   await setupCamera();
   video.play();
-
+  console.log("camera set up")
   model = await faceLandmarksDetection.load(
     faceLandmarksDetection.SupportedPackages.mediapipeFacemesh,
     {maxFaces: 1});
+  console.log("model configured - ready to detect")
   renderPrediction();
 };
 
